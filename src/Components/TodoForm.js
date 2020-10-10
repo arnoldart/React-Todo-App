739b35024702868c90454bbe71aef876701e41cc
adoni1209 <adoni.091203@gmail.com>
2020-10-10 10:06:07 +0700

    fix update

--- src/Components/TodoForm.js
+++ src/Components/TodoForm.js
@@ -1,8 +1,14 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 
 function TodoForm(props) {
 
   const [input, setInput] = useState('')
+
+  const inputRef = useRef(null)
+
+  useEffect(() => {
+    inputRef.current.focus()
+  })
 
   const HandleChange = e => {
     setInput(e.target.value)
@@ -29,6 +35,7 @@ function TodoForm(props) {
             value={input}
             onChange={HandleChange}
             autoComplete="off"
+            ref={inputRef}
           />
           <button className="todo-button">Add todo</button>
         </form>
